630a87d90f6b9874dc1ad6af8800cfc1fc68de00
shacharm2 <shacharm@gmail.com>
2018-11-20 18:37:42 +0200

    bugfixes

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -92,19 +92,19 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	for (unsigned int io = 0; io < observations.size(); io++)
 	{
 		double min_dist = 1e99;
-		int map_id;
+		int map_id = -1;
 		for (unsigned int ip = 0; ip < predicted.size(); ip++)
 		{
-			double dist = abs(predicted[ip].x - observations[io].x) + abs(predicted[ip].y - observations[io].y);
-			bool been_visited = find(visited.begin(), visited.end(), observations[io].id) != visited.end();
-			if ((dist < min_dist) & !been_visited)
+			//double dist = abs(predicted[ip].x - observations[io].x) + abs(predicted[ip].y - observations[io].y);
+			double distance = dist(predicted[ip].x, predicted[ip].y, observations[io].x, observations[io].y);
+			bool been_visited = find(visited.begin(), visited.end(), predicted[ip].id) != visited.end();
+			if ((distance < min_dist) & !been_visited)
 			{
-				min_dist = dist;
-				map_id = observations[io].id;
+				min_dist = distance;
+				map_id = predicted[ip].id;
 			}
 		}
 		visited.push_back(map_id);
-		//predicted[ip].id = map_id;
 		observations[io].id = map_id;
 	}
 }
@@ -131,7 +131,6 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 		double xp = this->particles[ip].x;
 		double yp = this->particles[ip].y;
 		double thetap = this->particles[ip].theta;
-		this->particles[ip].weight = 1;
 
 		// transformed observations
 		std::vector<LandmarkObs> observations_i;
@@ -152,16 +151,20 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 			float y = map_landmarks.landmark_list[im].y_f;; // Landmark y-position in the map (global coordinates)
 			
 			// take only landmarks R < sensor_range
-			double R = sqrt(pow(x-xp, 2) + pow(y-yp, 2));
+			double R = dist(x, y, xp, yp); //sqrt(pow(x-xp, 2) + pow(y-yp, 2));
 			if (R < sensor_range) {
 				predictions_i.push_back(LandmarkObs{id, x, y});
 			}
 		}
-
+		if (predictions_i.size() == 0)
+		{
+			this->particles[ip].weight = 1e-6;			
+			continue;
+		}
 		dataAssociation(predictions_i, observations_i);
 
 		// update weights
-		default_random_engine gen;		
+		this->particles[ip].weight = 1;
 		for (unsigned int io = 0; io < observations_i.size(); io++)
 		{
 			double xo = observations_i[io].x;
@@ -171,8 +174,8 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 			{
 				if (predictions_i[ip].id == observations_i[io].id)
 				{
-					xp = observations_i[io].x;
-					yp = observations_i[io].y;
+					xp = predictions_i[ip].x;
+					yp = predictions_i[ip].y;
 					break;
 				}
 			}
@@ -180,9 +183,9 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 			double dx = xp - xo;
 			double dy = yp - yo;
 			double exp_arg = -0.5 * (pow(dx / std_landmark[0], 2) + pow(dy / std_landmark[1], 2));
-			double gauss_norm = 1. / (2 * M_PI * std_landmark[0] * std_landmark[1]);
+			double gauss_norm = (2 * M_PI * std_landmark[0] * std_landmark[1]);
 			
-			this->particles[ip].weight *= gauss_norm * exp(exp_arg);
+			this->particles[ip].weight *= exp(exp_arg) / gauss_norm;
 			//this->particles[ip].weight *= normal_distribution<double>(xp - xo, std_landmark[0])(gen);
 			//this->particles[ip].weight *= normal_distribution<double>(yp - yo, std_landmark[1])(gen);
 
@@ -191,10 +194,19 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 		Wnorm += this->particles[ip].weight;
 	}
 
+	// if (Wnorm == 0)
+	// {
+	// 	for (unsigned int ip = 0; ip < (unsigned int)this->num_particles; ip++)
+	// 	{
+	// 		this->particles[ip].weight = 1;
+	// 	}		
+	// }
+	//cout << "Wnorm=" << Wnorm << endl;
+
 	// normalize
-	/*for (unsigned int ip = 0; ip < (unsigned int)this->num_particles; ip++){
-		this->particles[ip].weight /= Wnorm;
-	}*/
+	// for (unsigned int ip = 0; ip < (unsigned int)this->num_particles; ip++){
+	// 	this->particles[ip].weight /= Wnorm;
+	// }
 }
 
 void ParticleFilter::resample() {
@@ -205,12 +217,26 @@ void ParticleFilter::resample() {
 	// 1000 particles
 	std::array<int, NUM_PARTICLES> init = {0};
 	//int init[1000] = {0};
+	double min_weight = 1e99;
 	for (int i = 0; i < this->num_particles; i++)
 	{
-		init[i] = (int)(1000 * this->particles[i].weight);
+		min_weight = min(min_weight, this->particles[i].weight);
 	}
-	//{2,2,1,1,2,2,1,1,2,2};
+
+
+	for (int i = 0; i < this->num_particles; i++)
+	{
+		init[i] = (int)(this->particles[i].weight / min_weight);
+	}
 	std::discrete_distribution<int> vec(init.begin(), init.end());
+
+	std::vector<Particle> resampled_particles;
+	for (int i = 0; i < this->num_particles; i++)
+	{
+		resampled_particles.push_back(this->particles[vec(gen)]);
+	}
+	this->particles = resampled_particles;
+
 }
 
 Particle ParticleFilter::SetAssociations(Particle& particle, const std::vector<int>& associations, 
